c9246138dd54bf8269ad684852700856a96f4d0a
LinHan <60459607+linhan1999@users.noreply.github.com>
2021-02-25 20:34:08 +0800

    add hello world cxx by c++

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+
+  //  File Name: hello_world.cpp
+  //  Author: linhan
+  //  Mail: 593886981@qq.com 
+  //  Created Time: 四  2/25 20:32:44 2021
+ 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"hello world by github"<<endl;
+}
+
